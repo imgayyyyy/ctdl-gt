cee1ad07d171f795aca6828c987e40ee56874ebe
ptdat <98038583+imgayyyyy@users.noreply.github.com>
2025-06-25 15:21:37 +0700

    Update PTIT_CNTT5_IT201_Session04_Bai05.c

--- session04/PTIT_CNTT5_IT201_Session04_Bai05.c
+++ session04/PTIT_CNTT5_IT201_Session04_Bai05.c
@@ -1,26 +1,33 @@
 //
 // Created by DELL on 24/06/2025.
 //
-#include<stdio.h>
+#include <stdio.h>
 int main() {
     int n;
-    printf("nhap kich thuoc mang");
-    scanf("%d", &n);
+    printf("Enter n:");
+    scanf("%d",&n);
     int arr[n];
-    for(int i = 0; i < n; i++) {
-        printf("nhap phan tu arr[%d]:",i);
-        scanf("%d", &arr[i]);
+    for (int i=0;i<n;i++) {
+        printf(" arr[%d]:",i);
+        scanf("%d",&arr[i]);
     }
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+    int start=0;
+    int end=n-1;
+    int number;
+    printf("Enter number:");
+    scanf("%d",&number);
+    while (start<=end) {
+        int mid=(start+end)/2;
+        if (arr[mid]==number) {
+            printf("So %d tai vi tri arr[%d]",number,mid);
+            return 0;
+        }
+        if (arr[mid]<number) {
+            start=mid+1;
+        }
+        else {
+            end=mid-1;
         }
     }
-
     return 0;
 }
