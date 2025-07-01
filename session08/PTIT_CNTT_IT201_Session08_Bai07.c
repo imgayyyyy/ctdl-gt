5f8077c506f2db9ef76c3551e8aef399746a6682
ptdat <98038583+imgayyyyy@users.noreply.github.com>
2025-07-01 23:31:40 +0700

    Add files via upload

---
+++ session08/PTIT_CNTT_IT201_Session08_Bai07.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int left, int right) {
+    int pivot = arr[right];
+    int i = left - 1;
+
+    for (int j = left; j < right; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[right]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int left, int right) {
+    if (left < right) {
+        int pivotIndex = partition(arr, left, right);
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {9, 4, 7, 2, 10, 1, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Mảng ban đầu: ");
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Mảng sau khi sắp xếp (Quick Sort): ");
+    printArray(arr, n);
+
+    return 0;
+}
